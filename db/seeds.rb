404c7594175f9fe48bc6b91280c5b617cb7c7783
AngeloG1 <galasso.angelo@icloud.com>
2015-02-19 16:16:52 -0500

    seeded files

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,16 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
+   message = Message.create({
+			f_name: "Angelo",
+			l_name: "Galasso",
+			email: "A@gmail.com",
+			dob: Date.new,
+			phone_number: "860-945-897",
+			text_box: "I'm dying",
+			p_physician: 'Dr.B',
+			pain_level: 1
+			})
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
